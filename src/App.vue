01938fc70a4f2dc5f0c5bbed9227c47ab94ff879
沙志清 <shazhiqing@shazhiqingdeMacBook-Pro.local>
2024-01-06 09:26:23 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-view/>
+</template>
+
+<script setup>
+import {get} from  "@/http"
+import {ElMessage} from "element-plus";
+import {useStore} from "@/stores";
+import router from "@/router/index.js";
+
+const store = useStore()
+
+if(store.auth.user == null){
+  get("/api/user/get-user-info",(message)=> {
+    store.auth.user = message
+    router.push("/index")
+  }, ()=> {
+    store.auth.user = null
+  })
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="text-align: center; margin-top: 30vh">
+    <div style="margin: 10px; font-size: 20px">
+      登录成功
+    </div>
+    <div style="margin-top: 10px">
+      <el-button @click="logout" type="danger">退出登录</el-button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {get} from "@/http";
+import {ElMessage} from "element-plus";
+import router from "@/router/index.js";
+import {useStore} from "@/stores/index.js";
+
+const store = useStore();
+const logout = () => {
+  get('/api/auth/logout', (message) => {
+    ElMessage.success(message)
+    store.auth.user = null
+    router.push('/')
+  })
+}
+</script>
+
+
+<style scoped>
+
+</style>
